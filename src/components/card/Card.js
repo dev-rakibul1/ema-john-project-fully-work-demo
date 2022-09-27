e416fb9e9fa46df63d634449cf52c45f0885626b
dev-rakibul1 <fl.rakibul@gmail.com>
2022-09-27 06:16:29 +0600

    overal wark is done

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,10 +1,29 @@
 import React from "react";
+import "./Card.css";
 
 const Card = ({ cart }) => {
+  let total = 0;
+  let shipping = 0;
+  for (let product of cart) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+
+  // TOTAL TAX
+  const totalTax = (total * 0.1).toFixed(2);
+  const tax = parseFloat(totalTax);
+
+  //   GRAND TOTAL
+  const grandTotal = total + shipping + tax;
+
   return (
-    <div>
+    <div className="card-summary">
       <h4>Order summary</h4>
       <p>Select items : {cart.length}</p>
+      <p>Total Price: ${total} </p>
+      <p>Shipping charge: ${shipping}</p>
+      <p>Tax: ${tax}</p>
+      <h4>Total Grand: ${grandTotal}</h4>
       {/* <p>Total: $ {totalValue}</p> */}
     </div>
   );
--- src/components/header/shop/Shop.js
+++ src/components/header/shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getStoredCard } from "../../../utilities/fakedb";
 import Card from "../../card/Card";
 import Products from "../../products/Products";
 import "./Shop.css";
@@ -7,22 +8,40 @@ const Shop = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
   useEffect(() => {
+    // console.log("products loaded");
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  // console.log("products loaded after the console", products);
+
+  // stored card
+  useEffect(() => {
+    const storedCard = getStoredCard();
+    const savedCard = [];
+    for (let id in storedCard) {
+      const addedProducts = products.find((product) => product.id === id);
+      if (addedProducts) {
+        const quantity = storedCard[id];
+        addedProducts.quantity = quantity;
+        savedCard.push(addedProducts);
+      }
+    }
+    setCart(savedCard);
+  }, [products]);
+
+  console.log(cart);
 
   const addToCardHandler = (products) => {
     const newCart = [...cart, products];
     setCart(newCart);
+    addToDb(products.id);
   };
 
   const totalValue = cart.reduce(
     (newValue, currentValue) => newValue + currentValue.price,
     0
   );
-  console.log(totalValue);
-  console.log(cart);
 
   return (
     <div className="shop">
